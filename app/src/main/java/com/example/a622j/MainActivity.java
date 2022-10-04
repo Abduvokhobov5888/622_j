23d960bdc18dfdd735ce7b912844cb37ecda05ff
Abbosbek <vip.real.aa@gmail.com>
2022-10-04 16:30:58 +0500

    Java

---
+++ app/src/main/java/com/example/a622j/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.a622j;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    void initViews() {
+        EditText email1 = findViewById(R.id.email);
+        EditText pas = findViewById(R.id.pas);
+        Button save = findViewById(R.id.LogIn);
+        TextView textView = findViewById(R.id.signIn);
+
+        sharedPreferences = getSharedPreferences("userInfo", 0);
+
+            save.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String emailValue = email1.getText().toString();
+                    String passwordValue = pas.getText().toString();
+
+                    String registeredEmail = sharedPreferences.getString("emailValue", "");
+                    String registeredPassword = sharedPreferences.getString("passwordValue", "");
+
+                    if (email1.length() > 1 && pas.length() > 1) {
+                        if (emailValue.equals(registeredEmail) && passwordValue.equals(registeredPassword)) {
+                            Intent intent = new Intent(MainActivity.this, LastActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                        }
+                    }else {
+                        Toast.makeText(MainActivity.this, "Please, Sign in", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
